db9da41dd2461534da513fc236677ac0929484b9
Benjaminkerbi10 <131008126+Benjaminkerbi10@users.noreply.github.com>
2025-04-22 21:10:40 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -64,3 +64,6 @@ def chapters():
 def motivation():
     return render_template("motivation.html")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render or default to 5000
+    app.run(host="0.0.0.0", port=port)
